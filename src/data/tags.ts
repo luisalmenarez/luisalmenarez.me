69945764768e22e2e35af4371d793ad55db948ce
luisalmenarez <luisalmenarez0428@gmail.com>
2025-12-05 10:42:18 -0400

    refactor(Experience.astro, Projects.astro): streamline component data imports

---
+++ src/data/tags.ts
@@ -0,0 +1,70 @@
+import Twig from '@/components/icons/PhpIcon.astro';
+import Storybook from '@/components/icons/Storybook.astro';
+import Figma from '@/components/icons/Figma.astro';
+import JavaScript from '@/components/icons/JavaScript.astro';
+import SCSS from '@/components/icons/SCSS.astro';
+import React from '@/components/icons/React.astro';
+import Tailwind from '@/components/icons/Tailwind.astro';
+import Clerk from '@/components/icons/Clerk.astro';
+import Prisma from '@/components/icons/Prisma.astro';
+import Chart from '@/components/icons/Chart.astro';
+import NextJS from '@/components/icons/NextJS.astro';
+
+
+export const TAGS = {
+    NEXTJS: {
+      name: 'Next.js',
+      class: 'bg-black text-white',
+      icon: NextJS,
+    },
+    REACT: {
+      name: 'React',
+      color: 'text-red-300',
+      icon: React,
+    },
+    TAILWINDCSS: {
+      name: 'TailwindCSS',
+      class: 'bg-sky-500 text-white',
+      icon: Tailwind,
+    },
+    CLERK: {
+      name: 'Clerk',
+      class: 'bg-purple-800 text-white',
+      icon: Clerk,
+    },
+    PRISMA: {
+      name: 'Prisma',
+      class: 'bg-white text-black',
+      icon: Prisma,
+    },
+    RECHARTS: {
+      name: 'Recharts',
+      class: 'bg-blue-500 text-white',
+      icon: Chart,
+    },
+    TWIG: {
+      name: 'Twig',
+      class: 'bg-emerald-900 text-white',
+      icon: Twig,
+    },
+    STORYBOOK: {
+      name: 'Storybook',
+      class: 'bg-pink-600 text-white',
+      icon: Storybook,
+    },
+    FIGMA: {
+      name: 'Figma',
+      class: 'bg-purple-900 text-white',
+      icon: Figma,
+    },
+    JAVASCRIPT: {
+      name: 'JavaScript',
+      class: 'bg-yellow-300 text-black',
+      icon: JavaScript,
+    },
+    SCSS: {
+      name: 'SCSS',
+      class: 'bg-rose-400 text-white',
+      icon: SCSS,
+    },
+  };
